7bb0470663b6074c6ffe5db1e5105451f0541708
Lukas Weckner <lu.weckner@web.de>
2023-06-20 12:48:08 +0200

    display Button in preview cards

--- components/PreviewCard/index.js
+++ components/PreviewCard/index.js
@@ -1,30 +1,37 @@
 import { trips } from "../../lib/data";
 import styled from "styled-components";
+import Button from "../Button";
 
 export default function PreviewCard() {
-  const StyledListItem = styled.li`
-    border: 2px solid black;
-    border-radius: 1rem;
-    width: 90%;
-  `;
-  const StyledList = styled.ul`
-    list-style-type: none;
-    padding: 0;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 1rem;
-  `;
-
   return (
     <StyledList>
       {trips.map(({ slug, destination, startDate, endDate }) => (
         <StyledListItem key={slug}>
-          <h2>{destination}</h2>
-          <p>{startDate}</p>
-          <p>{endDate}</p>
+          <div>
+            <h2>{destination}</h2>
+            <p>{startDate}</p>
+            <p>{endDate}</p>
+          </div>
+          <Button>Show Details</Button>
         </StyledListItem>
       ))}
     </StyledList>
   );
 }
+
+const StyledList = styled.ul`
+  list-style-type: none;
+  padding: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1rem;
+`;
+
+const StyledListItem = styled.li`
+  border: 2px solid black;
+  border-radius: 1rem;
+  width: 90%;
+  display: flex;
+  justify-content: space-around;
+`;
