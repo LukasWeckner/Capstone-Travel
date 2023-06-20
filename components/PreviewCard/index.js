120d4792333923760f5e4199baa958b74bc47811
Lukas Weckner <lu.weckner@web.de>
2023-06-20 11:35:47 +0200

    create component PreviewCard, display it on index.js

--- components/PreviewCard/index.js
+++ components/PreviewCard/index.js
@@ -1,9 +1,30 @@
-export default function PreviewCard({ place, start, end }) {
+import { trips } from "../../lib/data";
+import styled from "styled-components";
+
+export default function PreviewCard() {
+  const StyledListItem = styled.li`
+    border: 2px solid black;
+    border-radius: 1rem;
+    width: 90%;
+  `;
+  const StyledList = styled.ul`
+    list-style-type: none;
+    padding: 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 1rem;
+  `;
+
   return (
-    <>
-      <h2>{place}</h2>
-      <p>{start}</p>
-      <p>{end}</p>
-    </>
+    <StyledList>
+      {trips.map(({ slug, destination, startDate, endDate }) => (
+        <StyledListItem key={slug}>
+          <h2>{destination}</h2>
+          <p>{startDate}</p>
+          <p>{endDate}</p>
+        </StyledListItem>
+      ))}
+    </StyledList>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
-import Heading from "../components/Heading";
-import SubHeading from "../components/SubHeading";
+import PreviewCard from "../components/PreviewCard";
 
 export default function Home() {
   return (
     <main>
       <h1>Current trips</h1>
+      <PreviewCard />
     </main>
   );
 }
