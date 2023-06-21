65eff089e6547dc7a892e09cd7475610f61783a2
Lukas Weckner <lu.weckner@web.de>
2023-06-21 15:08:03 +0200

    create new-trip patch which renders new-trip form

--- components/NewTripForm/index.js
+++ components/NewTripForm/index.js
@@ -1,11 +1,8 @@
 export default function NewTripForm() {
   return (
-    <>
-      <h1>New Trip</h1>
-      <form>
-        <label htmlFor="destination">Destination</label>
-        <input type="text" name="destination" id="destination" />
-      </form>
-    </>
+    <form>
+      <label htmlFor="destination">Destination</label>
+      <input type="text" name="destination" id="destination" />
+    </form>
   );
 }
---
+++ pages/new-trip/index.js
@@ -0,0 +1,10 @@
+import NewTripForm from "../../components/NewTripForm";
+
+export default function NewTrip() {
+  return (
+    <>
+      <h1>New Trip</h1>
+      <NewTripForm />
+    </>
+  );
+}
