a504eb5eaf34bca260da5a524635d49c1c806e36
Lukas Weckner <lu.weckner@web.de>
2023-06-21 15:40:29 +0200

    create Link to new-trip form

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,24 @@
 import PreviewCard from "../components/PreviewCard";
+import Link from "next/link";
+import styled from "styled-components";
 
 export default function Home() {
   return (
     <main>
       <h1>Current trips</h1>
+      <div>
+        <StyledLink href={"/new-trip"}>Create New Trip</StyledLink>
+      </div>
       <PreviewCard />
     </main>
   );
 }
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: black;
+  padding: 10px 20px;
+  background-color: #f2d5a3;
+  border-radius: 4px;
+  cursor: pointer;
+`;
