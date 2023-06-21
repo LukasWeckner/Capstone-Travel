912c9e108761db668f801aa9a173150b6efa34e4
Lukas Weckner <lu.weckner@web.de>
2023-06-21 17:10:06 +0200

    display all fields for the form

--- components/NewTripForm/index.js
+++ components/NewTripForm/index.js
@@ -1,8 +1,56 @@
+import styled from "styled-components";
+
 export default function NewTripForm() {
+  // default value till flexible trip duration will be implemented
+  const tripDurationInDays = 5;
+
+  // The inputs fields "Day title" and "Activities" need to be displayed based on the duration of the trip in days. So I use a loop here and call the function in line 45
+  function createMultipleDays() {
+    const tripDays = [];
+
+    for (let i = 0; i < tripDurationInDays; i++) {
+      tripDays.push(
+        <div>
+          <label htmlFor="title">
+            Day title:
+            <input type="text" name="title" id="title" />
+          </label>
+          <label htmlFor="activities">
+            Activities:
+            <textarea name="activities" id="activities"></textarea>
+          </label>
+        </div>
+      );
+    }
+    return tripDays;
+  }
+
   return (
     <form>
-      <label htmlFor="destination">Destination</label>
-      <input type="text" name="destination" id="destination" />
+      <StyledFieldSet>
+        <label htmlFor="destination">
+          Destination:
+          <input type="text" name="destination" id="destination" />
+        </label>
+        <label htmlFor="start-date">
+          Start date:
+          <input type="date" name="start-date" id="start-date" />
+        </label>
+        <label htmlFor="end-date">
+          End date:
+          <input type="date" name="end-date" id="end-date" />
+        </label>
+      </StyledFieldSet>
+      <h2 id="description">Trip Days</h2>
+      <fieldset aria-describedby="description">{createMultipleDays()}</fieldset>
     </form>
   );
 }
+
+const StyledFieldSet = styled.fieldset`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: flex-start;
+  gap: 5px;
+`;
