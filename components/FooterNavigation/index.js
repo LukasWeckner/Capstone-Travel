3d2101f8c81927add9f200170938f0b8b9638b4a
Lukas Weckner <lu.weckner@web.de>
2023-06-22 12:49:43 +0200

    add some styling to FooterNavigation

--- components/FooterNavigation/index.js
+++ components/FooterNavigation/index.js
@@ -1,12 +1,34 @@
 import Link from "next/link";
+import styled from "styled-components";
 
 // I intentially didn't add a <footer> tag around the <nav> here in the component, so that every single page of my app uses a clear semantic structure of <header>, <main> and <footer>. Meaning the FooterNavigation component is surrounded by <footer> tags on every page of the app.
 
 export default function FooterNavigation() {
   return (
-    <nav>
-      <Link href={"/"}>My trips</Link>
-      <Link href={"/new-trip"}>New trip</Link>
-    </nav>
+    <StyledNavWrapper>
+      <StyledNav>
+        <StyledLink href={"/"}>My trips</StyledLink>
+        <StyledLink href={"/new-trip"}>New trip</StyledLink>
+      </StyledNav>
+    </StyledNavWrapper>
   );
 }
+
+const StyledNavWrapper = styled.div`
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  padding: 1rem;
+  background-color: grey;
+`;
+
+const StyledNav = styled.nav`
+  display: flex;
+  justify-content: space-evenly;
+  gap: 2rem;
+`;
+
+const StyledLink = styled(Link)`
+  color: white;
+  text-decoration: none;
+`;
