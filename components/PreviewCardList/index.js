24f7e66dab84df90e5a34b5603195ec6f6883a01
Lukas Weckner <lu.weckner@web.de>
2023-06-27 18:47:24 +0200

    minor styling

--- components/PreviewCardList/index.js
+++ components/PreviewCardList/index.js
@@ -63,6 +63,7 @@ const StyledButton = styled(StyledBasicButton)`
 const StyledLink = styled(Link)`
   text-decoration: none;
   padding: 0.5rem 1rem;
+  color: black;
   background-color: #f2d5a3;
   border-radius: 4px;
   cursor: pointer;
