691ec75c4cfbf13933da40db1d8d8dc4d8c3aa8c
Lukas Weckner <lu.weckner@web.de>
2023-07-01 19:16:01 +0200

    add Idea Generator page to footer nav

--- components/FooterNavigation/index.js
+++ components/FooterNavigation/index.js
@@ -12,10 +12,13 @@ export default function FooterNavigation() {
       <StyledNav>
         {/*router.asPath identifies the current pathname of the page you are currently on */}
         <StyledLink href={"/"} pathname={router.asPath}>
-          My trips
+          My Trips
+        </StyledLink>
+        <StyledLink href={"/idea-generator"} pathname={router.asPath}>
+          Idea Generator
         </StyledLink>
         <StyledLink href={"/new-trip"} pathname={router.asPath}>
-          New trip
+          New Trip
         </StyledLink>
       </StyledNav>
     </StyledNavWrapper>
@@ -30,26 +33,17 @@ const StyledNav = styled.nav`
   position: fixed;
   bottom: 0;
   width: 100%;
-  padding: 1rem;
+  padding: 1.5rem 0.5rem;
   background-color: grey;
   display: flex;
-  justify-content: space-evenly;
+  justify-content: space-around;
   gap: 2rem;
 `;
 
 const StyledLink = styled(Link)`
-  // check react-styled-components handout in section "Adapting based on props" to understand why I used a template literal here. I destructured the props of StyledLink like in the last line of example code in the handouts section.
-  color: ${({ href, pathname, children }) =>
-    //Checks if the href prop of the link equals the current pages path.
-    href === pathname ||
-    /*  I use the or-operator || because I also want to apply the color "#f2d5a3" to the my "My trips" navigation item when a user is on a details page. The first condition "href === pathname" of line 43 doesn't cover this case because the pathname won't equal the href.
-    
-    As my details pages are the only pages which start with the pathname "/my-trips/" I set this as a condition. The "&& children === "My trips"" additionally checks if the name of my navigation item is "My trips" because else both navigation items would be colored with "#f2d5a3".
-    */
-    (pathname.startsWith("/my-trips/") && children === "My trips")
-      ? "orange"
-      : "black"};
   text-decoration: none;
-  font-size: 1.5rem;
+  font-size: 1.1rem;
   font-weight: bold;
+  //Check if the href prop of the link equals the current pages url-path.
+  color: ${({ href, pathname }) => (href === pathname ? "orange" : "black")};
 `;
