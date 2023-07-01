dfbdd4a6df53971e7a0d77b010fee7048ef92ea8
Lukas Weckner <lu.weckner@web.de>
2023-07-01 18:42:30 +0200

    create idea-generator page

---
+++ pages/idea-generator/index.js
@@ -0,0 +1,16 @@
+import FooterNavigation from "../../components/FooterNavigation";
+import Header from "../../components/Header";
+
+export default function IdeaGenerator() {
+  return (
+    <>
+      <header>
+        <Header heading="Idea Generator" />
+      </header>
+      <main></main>
+      <footer>
+        <FooterNavigation />
+      </footer>
+    </>
+  );
+}
