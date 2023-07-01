e92429f489c17ede1ee4ad4dcb89bad5fc5591bd
Lukas Weckner <lu.weckner@web.de>
2023-07-01 11:04:33 +0200

    remove not needed imports

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,5 @@
-import { useState } from "react";
 import GlobalStyle from "../styles";
 import Head from "next/head";
-import { trips } from "../lib/data";
 import useLocalStorageState from "use-local-storage-state";
 
 export default function App({ Component, pageProps }) {
