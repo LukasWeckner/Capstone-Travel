323df095d968a611f5141a2c0af3739939e12e25
Lukas Weckner <lu.weckner@web.de>
2023-07-02 09:49:00 +0200

    get some output from api on backend route /api/hello

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,26 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import openai from "../../utils/openai.js";
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(request, response) {
+  try {
+    const chatCompletion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "user",
+          content: "Create a 3 day schedule for a vacation in rome",
+        },
+      ],
+    });
+
+    const responseText = chatCompletion.data.choices[0].message.content;
+    response.status(200).json(responseText);
+  } catch (error) {
+    if (error.response) {
+      console.log(error.response.status);
+      console.log(error.response.data);
+    } else {
+      console.log(error.message);
+    }
+  }
 }
