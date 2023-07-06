39d7c6d0333d8ac8782171af9d0d9ce8d91572da
Lukas Weckner <lu.weckner@web.de>
2023-07-06 16:45:12 +0200

    Create component FormContainer

---
+++ components/StyledComponents/FormContainer/index.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const FormContainer = styled.div`
+  margin: auto;
+  margin-top: 2.5rem;
+  background: #fff;
+  border-radius: 1rem;
+  width: 90%;
+  padding: 0.5rem 1rem;
+  box-shadow: 2px 5px 10px -7px #000000, 0px -2px 10px -7px #000000;
+`;
--- pages/idea-generator/index.js
+++ pages/idea-generator/index.js
@@ -8,6 +8,7 @@ import { ContainerCenterElement } from "../../components/NewTripForm";
 import { StyledBasicButton } from "../../components/Button";
 import { StyledLabel } from "../../components/StyledComponents/StyledLabel";
 import { GridForm } from "../../components/StyledComponents/GridForm";
+import { FormContainer } from "../../components/StyledComponents/FormContainer";
 import { useRouter } from "next/router";
 import { format } from "date-fns";
 import Image from "next/image";
@@ -160,15 +161,6 @@ const StyledText = styled.p`
 `;
 
 //form
-const FormContainer = styled.div`
-  margin: auto;
-  margin-top: 2.5rem;
-  background: #fff;
-  border-radius: 1rem;
-  width: 90%;
-  padding: 0.5rem 1rem;
-  box-shadow: 2px 5px 10px -7px #000000, 0px -2px 10px -7px #000000;
-`;
 
 const StyledInput = styled.input`
   height: 1.5rem;
